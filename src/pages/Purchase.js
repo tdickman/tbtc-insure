5c7d350b124a5c114957502c8ac2758b598dd025
Tom Dickman <tdickman@gmail.com>
2020-09-30 22:18:35 -0400

    Updated to support dai

--- src/pages/Purchase.js
+++ src/pages/Purchase.js
@@ -128,7 +128,10 @@ function Quote(props) {
     setButtonIsEnabled(false)
     const response = await fetch(`${NM_URL}/v1/quote?coverAmount=${props.amount}&currency=${props.currency}&period=${props.coverageDays}&contractAddress=0x9424B1412450D0f8Fc2255FAf6046b98213B76Bd`)
     const json = await response.json()
+    //const json = {"currency":"ETH","period":"50","amount":"50","price":"355920602327173169","priceInNXM":"3452257870084029366","expiresAt":1601516079,"generatedAt":1601515778149,"contract":"0xe20a5c79b39bc8c363f0f49adcfa82c2a01ab64a","v":27,"r":"0x209cd31ef9fe92d51a37d76466b1113bfeb5e702d383fd19f30a32efd1c41ade","s":"0x6d4e270d9e798e984cf5f61a3fc4f6f42f40673088ca00d24603d5b866e50d07"}
     // let json = {"currency":"DAI","period":"30","amount":"2","price":"8542094455852156","priceInNXM":"80041487420872383","expiresAt":1601426633,"generatedAt":1601426332150,"contract":"0x9424b1412450d0f8fc2255faf6046b98213b76bd","v":28,"r":"0xaa4b19165798648294774c5597f4e1c53dcdb6131b7d2e08646170aa0c946302","s":"0x121477b8d7d598d82243e4bbfe2687ada51513ce68da783ef7a15293a98add54"}
+    //const json = {"currency":"ETH","period":"50","amount":"50","price":"355920602327173169","priceInNXM":"3452257870084029366","expiresAt":1601516204,"generatedAt":1601515903517,"contract":"0x9424b1412450d0f8fc2255faf6046b98213b76bd","v":28,"r":"0xe19a6959dce33960c71d912f1cc793432d88df15187a89822ca5ff9b9cdfa18a","s":"0x3e15fc0bb838a44c37636263bcfd61a3cf5ee35c3ed0a17a2c21f493fcaac77e"}
+    //const json = {"currency":"ETH","period":"50","amount":"50","price":"355920602327173169","priceInNXM":"3452257870084029366","expiresAt":1601517523,"generatedAt":1601517222567,"contract":"0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f","v":28,"r":"0x1d5590dc833740212ba80970153d4ede6cdc52c5757824771ec102cbcbe69b1a","s":"0x19b82a293d89cd10a562f59035b787841fc762fc44b6425ba68449f75a4ebd82"}
     console.log(json)
     setQuote(json)
   }
@@ -136,8 +139,13 @@ function Quote(props) {
   async function purchaseCoverage() {
     console.log('purchasing coverage')
     const coverDetails = [quote.amount, quote.price, quote.priceInNXM, quote.expiresAt, quote.generatedAt]
+    let sendValue = undefined
+    if (quote.currency === 'ETH') {
+      sendValue = quote.price
+    }
+
     const sendSymbol = Web3Utils.asciiToHex(quote.currency) + '00'
-    yinsureContract.buyCover(quote.contract, '0x44414900', coverDetails, quote.period, quote.v, quote.r, quote.s)
+    yinsureContract.buyCover(quote.contract, sendSymbol, coverDetails, quote.period, quote.v, quote.r, quote.s, {value: sendValue})
   }
 
   return (
