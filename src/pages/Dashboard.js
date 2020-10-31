befe776b00b6502c1aff5c8b2b9324e39eb0fae5
Tom Dickman <tdickman@gmail.com>
2020-10-31 13:10:59 -0400

    Add ability to submit claim

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -10,7 +10,6 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 import fetch from 'node-fetch';
 
@@ -31,6 +30,11 @@ function MyTokens(props) {
 
   const [tokens, setTokens] = React.useState([])
 
+  async function handleSubmitClaim(tokenId) {
+    console.log(tokenId)
+    yinsureContract.submitClaim(tokenId)
+  }
+
   React.useEffect(() => {
     async function fetchData() {
       const address = await signer.getAddress()
@@ -43,7 +47,8 @@ function MyTokens(props) {
         var expirationDate = new Date(0)
         expirationDate.setUTCSeconds(tokenDetails.expirationTimestamp)
         tempTokens.push({
-          id: tokenDetails.coverId.toNumber(),
+          id: token.toNumber(),
+          coverId: tokenDetails.coverId.toNumber(),
           coverageAmount: tokenDetails.coverAmount.toNumber(),
           coverageCurrency: tokenDetails.coverCurrency === "0x45544800" ? "ETH" : "DAI",
           expiration: expirationDate.toString(),
@@ -68,6 +73,7 @@ function MyTokens(props) {
                 <li>Covered Contract: <a target="_blank" href={`https://etherscan.io/address/${t.coveredAddress}`}>{t.coveredAddress}</a></li>
               </ul>
             </Card.Text>
+            <Button onClick={() => handleSubmitClaim(t.id)} variant="primary">Submit Claim</Button>
           </Card.Body>
         </Card>
       )}
